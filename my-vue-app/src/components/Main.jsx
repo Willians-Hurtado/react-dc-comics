2559e75a527c5fa27faa097c20d66acd67a42dbb
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-20 15:21:52 +0100

    add main

--- my-vue-app/src/components/Main.jsx
+++ my-vue-app/src/components/Main.jsx
@@ -2,14 +2,42 @@ export default function Main() {
 
 
     return (
-        <main>
-            <section>
+        <main className="">
+            <section className="d-flex align-items-center bg-dark">
+                <h1>
+                    Content goes here
+                </h1>
+            </section>
+            <section className="comics d-flex justify-content-center align-items-center">
+                <div className="content ">
+                    <img src="/buy-comics-digital-comics.png" alt="" />
+                    <span>DIGITAL COMICS</span>
+                </div>
+
+                <div className="content ">
+                    <img src="/buy-comics-merchandise.png" alt="" />
+                    <span>DC MERCHANDISE</span>
+                </div >
+
+                <div className="content ">
+                    <img src="/buy-comics-shop-locator.png" alt="" />
+                    <span>SUBSCRIPTION</span>
+                </div>
+
+
+                <div className="content ">
+                    <img src="/buy-comics-subscriptions.png" alt="" />
+                    <span>COMIC SHOP LOCATOR</span>
+                </div>
+                <div className="content ">
+                    <img src="/buy-dc-power-visa.svg" alt="" />
+                    <span>DC POWER VISA</span>
+                </div>
+
+
 
             </section>
-            <section>
-
-            </section>
-        </main>
+        </main >
     )
 
 }

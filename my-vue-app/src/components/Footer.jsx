4abf5458336e396b02075670066c68990075cbc4
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-20 14:47:46 +0100

    add header

---
+++ my-vue-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+
+
+    return (
+        <footer>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
+        </footer>
+    )
+
+}
---
+++ my-vue-app/src/components/Header.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+
+
+    return (
+        <header>
+            <div className="d-flex justify-content-center align-items-center justify-content-between">
+                <img src="/dc-logo.png" alt="" />
+                <ul className="d-flex ">
+                    <li>CHARACTER</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </div>
+
+        </header>
+    )
+
+}

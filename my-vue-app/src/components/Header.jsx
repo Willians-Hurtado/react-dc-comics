77290c6bc3b46a36d2f6b3307709e1c1978e2b2d
Willians-Hurtado <willianshurtado5@gmail.com>
2025-03-24 12:31:32 +0100

    fix nav bar

--- my-vue-app/src/components/Header.jsx
+++ my-vue-app/src/components/Header.jsx
@@ -3,17 +3,20 @@ export default function Header({ links }) {
 
     return (
         <header>
-            <div className="nav-bar d-flex justify-content-center align-items-center justify-content-between ">
+            <div className="nav-bar d-flex  align-items-center justify-content-between ">
                 <img src="/dc-logo.png" alt="" />
 
-                {links.map((link, index) => (
-                    <ul>
-                        <li>
-                            <a key={index} href={`#${link.toLowerCase()}`} className={index === 1 ? "nav-link special-link" : "nav-link"}>{link}</a>
-                        </li>
-                    </ul>
+                <div className="menu d-flex align-items-center"  >
+                    {links.map((link, index) => (
+                        <ul>
+                            <li>
+                                <a key={index} href={`#${link.toLowerCase()}`} className={index === 1 ? "nav-link special-link" : "nav-link"}>{link}</a>
+                            </li>
+                        </ul>
 
-                ))}
+                    ))}
+                </div>
+
             </div>
 
             <section>
